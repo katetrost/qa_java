626fda40820642b09c30f24a6eb3db74b5d9c6a1
katetrost <katerine.trost@gmail.com>
2021-11-09 00:13:00 +0500

    вынесла переменную feline в @Before

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,16 +1,23 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 
 public class FelineTest {
+    private Feline feline;
+
+    @Before
+    public void initObjects() {
+        feline = new Feline(); // создали экземпляр класса
+    }
 
     @Test
     public void getFamilyTest() {
-        Feline feline = new Feline(); // создали экземпляр класса
         String actual = feline.getFamily();
         String expected = "Кошачьи";
         assertEquals(expected, actual); // сравнили ожидаемый результат с фактическим
@@ -18,7 +25,6 @@ public class FelineTest {
 
     @Test
     public void getKittensTestDefault() {
-        Feline feline = new Feline(); // создали экземпляр класса
         int actual = feline.getKittens();
         int expected = 1;
         assertEquals(expected, actual); // сравнили ожидаемый результат с фактическим
@@ -26,7 +32,6 @@ public class FelineTest {
 
     @Test
     public void getKittensTestWithParams() {
-        Feline feline = new Feline();
         int actual = feline.getKittens(2);
         int expected = 2;
         assertEquals(expected, actual);
@@ -34,7 +39,6 @@ public class FelineTest {
 
     @Test
     public void eatMeatTest() throws Exception {
-        Feline feline = new Feline();
         List<String> actual = feline.eatMeat();
         List<String> expected = List.of("Животные", "Птицы", "Рыба");
         assertEquals(expected, actual);
